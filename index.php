5752912abace8779808070098491a21e136afdda
Paxton Bigler <paxton.bigler@gmail.com>
2017-02-09 19:18:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+*******************REVERSE WORDS IN A STRING********************************
+You need to write a function that reverses the words in a given string. A word can also fit an empty string. 
+If this is not clear enough, here are some examples:
+reverse("Hello World") === "World Hello"
+reverse("Hi There.") === "There. Hi"
+Happy coding!
 
+function reverse($string) {
+  $array = explode(" ", $string);
+  $reversed = array_reverse($array);
+  return implode(" ", $reversed);
+}
